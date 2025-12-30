bbe7b55048aa86072ade973dec2046209d964759
Kartik B <bansodekartik00@gmail.com>
2025-12-30 12:53:19 +0530

    bug fix

---
+++ scripts/auth.py
@@ -0,0 +1,16 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret.json",
+    SCOPES
+)
+
+creds = flow.run_local_server(port=0)
+
+with open("token.json", "w") as token:
+    token.write(creds.to_json())
+
+print("token.json created successfully")
